450daae9dba08a82d5896231f39537520f6aa6dc
ACignoni2 <alessio.cignoni@gmail.com>
2025-03-12 14:37:00 +0100

    first implementaion for tictactoe app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export default function Square() {
+
+  console.log("Sono qui!")
+
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
